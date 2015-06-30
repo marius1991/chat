cfcc39d0585ca0b5f49c68fac086e0a2c1edac01
raxakk <phi.ri@gmx.de>
2015-06-30 14:38:43 +0200

    Excepition

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,7 +18,7 @@ class MessagesController < ApplicationController
   def create
     begin
       sha256 = OpenSSL::Digest::SHA256.new
-      digest = sha256.hexdigest(params[:timestamp] + params[:recipientname] + params[:name] + params[:cipher] + params[:iv] + params[:key_recipient_enc] + params[:sig_recipient])
+      digest = sha256.hexdigest(params[:timestamp].to_s + params[:recipientname].to_s + params[:name].to_s + params[:cipher].to_s + params[:iv].to_s + params[:key_recipient_enc].to_s + params[:sig_recipient].to_s)
       public_key = OpenSSL::PKey::RSA.new(Base64.decode64(User.find_by_name(params[:name]).public_key))
       decrypt_digest = public_key.public_decrypt(Base64.decode64(params[:sig_service]))
 
@@ -41,7 +41,7 @@ class MessagesController < ApplicationController
           end
         else
           respond_to do |format|
-            format.json { render json: @success = '{"status":"' + params[:timestamp] + params[:recipientname] + params[:name] + params[:cipher] + params[:iv] + params[:key_recipient_enc] + params[:sig_recipient] + 'Unser Digest:' + digest.to_s + 'Marians Digest' + params[:sig_service].to_s  +  '"}'}
+            format.json { render json: @success = '{"status":"' + params[:timestamp].to_s + params[:recipientname].to_s + params[:name].to_s + params[:cipher].to_s + params[:iv].to_s + params[:key_recipient_enc].to_s + params[:sig_recipient].to_s + 'Unser Digest:' + digest.to_s + 'Marians Digest' + params[:sig_service].to_s  +  '"}'}
           end
         end
       else
